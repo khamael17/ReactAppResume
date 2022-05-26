6637987641b38e8543e15db4fbe09ae9b98dd2c2
Khalilou Konate <KhalilKo@1stSourceCorp.com>
2022-05-26 15:51:25 -0400

    ok

---
+++ build/precache-manifest.a654dad93041874e92adde07ba71b58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0ac6f6c7d37eb81bcbd59f4503a5f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c0eed06fff1c876f1d",
+    "url": "/static/css/main.23ec6bf5.chunk.css"
+  },
+  {
+    "revision": "aca3ec2212f4c28480dd",
+    "url": "/static/js/2.465581b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.465581b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c0eed06fff1c876f1d",
+    "url": "/static/js/main.3a5b2a0b.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
